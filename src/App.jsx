dd65488a77e54b00be0d3cfbf7c4950c2065cbae
miguelcc17 <carlosechenique123m@gmail.com>
2025-02-20 22:17:19 -0400

    [feat]: add status of character in cards component

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
             <img className="card-img-top" src={item.image} alt="Card image cap"/>
             <div className="card-body">
               <h5 className="card-title">{item.name}</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p className="card-text">{item.status}</p>
             </div>
           </div>
           ))}
